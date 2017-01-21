f115eac0d5f2270ab75963bef5218d21caaf0c56
epopoff <pop.off.evgeny@gmail.com>
2017-01-21 17:34:17 +0300

    Перенес старый проект

---
+++ hello.py
@@ -0,0 +1,9 @@
+def app (environ, start_response):
+	resp = environ['QUERY_STRING'].split('&')
+	resp = [item + '\r\n' for item in resp]
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	start_response(status, headers)
+	return resp
---
+++ init.sh
@@ -0,0 +1,6 @@
+# mkdir -p ~/web/public/img ~/web/public/css ~/web/public/js ~/web/uploads ~/web/etc
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/gunicorn.conf
+sudo /etc/init.d/gunicorn restart
